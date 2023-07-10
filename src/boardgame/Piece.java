525e1dbe9de69000bd8098f1754e5cdce64b419c
4cruzeta <4cruzetam@gmail.com>
2023-07-10 13:36:31 -0300

    Movimentos possíveis de uma peça

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -1,6 +1,6 @@
 package boardgame;
 
-public class Piece {
+public abstract class Piece {
 
 	protected Position position; // atributo position protected para não ser 
 								// visivel na camada xadrez.
@@ -13,6 +13,24 @@ public class Piece {
 
 	protected Board getBoard() { // protected apenas uso interno - tabuleiro.
 		return board;
+			
 	}
-		
+	
+	public abstract boolean[][] possibleMoves();
+
+	public boolean possibleMove(Position position) {
+		return possibleMoves()[position.getRow()][position.getColumn()];
+	}
+
+	public boolean isThereAnyPossibleMove() {
+		boolean[][] mat = possibleMoves();
+		for (int i=0; i<mat.length; i++) {
+			for (int j=0; j<mat.length; j++) {
+				if (mat[i][j]) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
 }
